6522474f29855d593de58aa22856791104f2c24c
DanielB <bae@supernode.ai>
2023-03-14 07:08:03 +0000

    Update GlobalData.js and GlobalMap.js

--- src/Components/GlobalData/GlobalData.js
+++ src/Components/GlobalData/GlobalData.js
@@ -1,28 +1,32 @@
-import React, {useState, useEffect} from "react";
-import {sortBy, reverse, path} from "ramda";
-import NumberFormat from 'react-number-format';
+import React, { useState, useEffect } from "react";
+import { sortBy, reverse, path } from "ramda";
+import NumberFormat from "react-number-format";
 import { getData } from "country-list";
 import GlobalMap from "../GlobalMap/GlobalMap";
 import "./GlobalData.css";
 
 const GlobalData = () => {
-  const [covidData, setCovidData] = useState([])
+  const [covidData, setCovidData] = useState([]);
   useEffect(() => {
-    fetch('https://pomber.github.io/covid19/timeseries.json')
-    .then(res => res.json())
-    .then(res => getData().map(d => {
-        if (res[d.name]) {
-          d.stats = res[d.name].reverse()[0];
-        }
-        return d;
-      }))
-      .then(data => setCovidData(reverse(sortBy(path(["stats", "confirmed"]), data))))
-  }, [])
+    fetch("https://dan1elb.github.io/kpop-index/data/timeseries.json")
+      .then((res) => res.json())
+      .then((res) =>
+        getData().map((d) => {
+          if (res[d.name]) {
+            d.stats = res[d.name].reverse()[0];
+          }
+          return d;
+        })
+      )
+      .then((data) =>
+        setCovidData(reverse(sortBy(path(["stats", "confirmed"]), data)))
+      );
+  }, []);
 
   return (
     <div>
       <GlobalMap covidData={covidData} />
     </div>
-) 
-}
+  );
+};
 export default GlobalData;
--- src/Components/GlobalMap/GlobalMap.js
+++ src/Components/GlobalMap/GlobalMap.js
@@ -1,60 +1,56 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { VectorMap } from "react-jvectormap";
 import { getData } from "country-list";
 import "./GlobalMap.css";
 
+const handleClick = (e, countryCode) => {
+  console.log(countryCode);
+};
 
-  const handleClick = (e, countryCode) => {
-    console.log(countryCode);
-  };
+const GlobalMap = () => {
+  const [countryCodeStateObj, setCountryCodeObj] = useState({});
 
-  const GlobalMap = () => {
+  useEffect(() => {
+    const makeAPICall = async () => {
+      const res = await fetch(
+        "https://dan1elb.github.io/kpop-index/data/timeseries.json"
+      );
 
-    const [countryCodeStateObj,setCountryCodeObj] = useState({})
+      const json = await res.json();
+      const countryCodes = getData().map((d) => {
+        if (json[d.name]) {
+          d.stats = json[d.name].reverse()[0];
+        }
+        return d;
+      });
 
-    useEffect(() => {
-      const makeAPICall = async () => {
-        const res = await fetch(
-          'https://pomber.github.io/covid19/timeseries.json'
-        );
+      let countryCodeStats = {};
+      countryCodes.map((country) => {
+        if (country.stats !== undefined) {
+          return (countryCodeStats[country.code] = country.stats.confirmed);
+        } else {
+          return (countryCodeStats[country.code] = 0);
+        }
+      });
+      setCountryCodeObj(countryCodeStats);
+    };
+    makeAPICall();
+  }, []);
 
-       const json = await res.json();
-        const countryCodes = getData().map(d => {
-          if (json[d.name]) {
-            d.stats = json[d.name].reverse()[0];
-          }
-          return d;
-        });
+  console.log(countryCodeStateObj);
 
-        
-        let countryCodeStats = {}
-         countryCodes.map( country => {
-          if(country.stats !== undefined) {
-            return countryCodeStats[country.code] = country.stats.confirmed
-          } else {
-            return countryCodeStats[country.code] = 0
-          }
-        })
-        setCountryCodeObj(countryCodeStats)
-        
-      };
-      makeAPICall();
-    }, []);
-
-console.log(countryCodeStateObj)
-
-    return (
+  return (
     <>
-    <VectorMap
+      <VectorMap
         map={"world_mill"}
         markers={{
-          US: { latLng: [37.9, -88.45], name: 'Some' },
-          CO: { latLng:[4.624335, -74.063644],name:'Bogota'}
+          US: { latLng: [37.9, -88.45], name: "Some" },
+          CO: { latLng: [4.624335, -74.063644], name: "Bogota" },
         }}
         backgroundColor="transparent"
         zoomOnScroll={true}
         onRegionClick={handleClick}
-        onMarkerClick={(e)=> console.log(e)}
+        onMarkerClick={(e) => console.log(e)}
         containerClassName="map"
         regionStyle={{
           initial: {
@@ -63,11 +59,11 @@ console.log(countryCodeStateObj)
             stroke: "none",
             "stroke-width": 0,
             "stroke-opacity": 0,
-            cursor: "pointer"
+            cursor: "pointer",
           },
-          hover: { 
+          hover: {
             "fill-opacity": 0.8,
-            cursor: "pointer"
+            cursor: "pointer",
           },
           selected: {
             fill: "#212121",
@@ -76,7 +72,7 @@ console.log(countryCodeStateObj)
           },
           selectedHover: {
             "fill-opacity": 0.8,
-          }
+          },
         }}
         regionsSelectable={true}
         series={{
@@ -84,9 +80,9 @@ console.log(countryCodeStateObj)
             {
               values: countryCodeStateObj,
               scale: ["#2155CD", "#79DAE8"],
-              normalizeFunction: "polynomial"
-            }
-          ]
+              normalizeFunction: "polynomial",
+            },
+          ],
         }}
       />
     </>
